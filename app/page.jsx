a38b2eba8f257aac0479195237b96810aaf1a682
unknown <hallysust@gmail.com>
2023-08-15 13:42:38 +0600

    use Client remove from homepage

--- app/page.jsx
+++ app/page.jsx
@@ -1,20 +1,16 @@
-"use client"
-import { useRouter } from 'next/navigation';
+
 import Menu from '@/components/Menu';
 import Image from 'next/image';
 import React from 'react';
 
 const Page = () => {
-  const router = useRouter()
-  const goto = (path)=> {
-    router.push(path)
-  }
+ 
   return (
     <div className="container mx-auto">
       <Menu />
       <h1>This is Home page prod</h1>
       <div className="m-4">
-        <button onClick={()=> {goto('/posts')}} className="btn py-2 px-4 bg-blue-400 rounded-lg ">Goto Post</button>
+        {/* <button onClick={()=> {goto('/posts')}} className="btn py-2 px-4 bg-blue-400 rounded-lg ">Goto Post</button> */}
       </div>
       <div>
         {/* <img src="/images/laptop.jpg" alt="" /> */}
